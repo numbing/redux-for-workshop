6d67686e5ea8652dbd39cd82e455fda293c25f95
mir <mirnumbing@gmail.com>
2019-07-29 17:11:55 +0200

    install packages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <div>i am redux</div>
+      <h1>i am redux</h1>
     </div>
   );
 }
